e23ff27d1a605def07a42b8ae8bd41779d5b7553
mebin jacob <mebinjacob@gmail.com>
2015-08-13 05:45:53 -0400

    File download manager

--- download.py
+++ download.py
@@ -1,8 +1,11 @@
 import requests
 import os
 
-def download(url):
-    requests.get(url)
+def download(dir):
+    url = 'http://s3.amazonaws.com/aws-publicdatasets/trec/kba/kba-streamcorpus-2014-v0_3_0-serif-only/'+ dir +'/index.html'
+    req = requests.get(url)
+    print req.content
+
 def getListOfDirDownloaded(direcotry):
     file=open('filesDownloaded.txt', 'w+')
     directoryList = [ name for name in os.listdir(direcotry) if os.path.isdir(os.path.join(direcotry, name)) ]
@@ -24,8 +27,10 @@ directoryList = ['/data/d01/kba2013/aws-publicdatasets/trec/kba/kba-streamcorpus
                  '/data/d02/kba2013/aws-publicdatasets/trec/kba/kba-streamcorpus-2013-v0_2_0-english-and-unknown-language']
 listOfFilesDownloaded=[]
 for directory in directoryList:
-    listOfFilesDownloaded.append(getListOfDirDownloaded(directory))
+    listOfFilesDownloaded.extend(getListOfDirDownloaded(directory))
 
 
 toDownloadSet = set(getListOfDirToDownload()).difference(set(listOfFilesDownloaded))
-print toDownloadSet
+for dir in toDownloadSet:
+    download(dir)
+    break
